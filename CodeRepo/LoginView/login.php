1adc885d98e8f8f0cc41a848ead3600508591b76
sashort <95879519+sashort@users.noreply.github.com>
2023-01-30 15:17:24 -0500

    Added dummy getMods DB function
    Added dummy getMods DB function required to collect applied mods to a menu item.
    
    Introduced a loggegIn( username ) database function to check if user is logged in... Will throw an error is username is invalid.
    
    Fixed login window. You won't be able to incorrectly see the role dropdown after you enter you username and password, then press "Select Role", but you are already logged in.

---
+++ CodeRepo/LoginView/login.php
@@ -0,0 +1,150 @@
+<?php
+	include '../Resources/php/connect_disconnect.php';
+	connection();
+	$sql = NULL;
+	$allowedRoles = NULL;
+	$errorMessage = NULL;
+	
+	$cookie_name = "804288a34eb7a49b349be68fc6437621cbf25e10d82f4268bb795eca277adedb6a3367add5bfb7cbffb50df150e2e78d26b276f37d32d96cd76746065df58a30cde25c4d9803aa7214dc8f6a985bf8643c341f229b5834964b0f371915d5677e4b579fbab42844cd63ddc3148e4250591277cfc521906bc30cfedd765974c2009ae5fe451ab1890e5ebbfa120ad18934c972618dbe3e";;
+	
+	//attempt to get allowed roles if unverifed username has been submitted
+	if (isset($_POST['uname'])) {
+		//if username exists, get the allowed roles value
+		$sql = "SELECT roleLevel FROM Employees WHERE userName = '" .$_POST['uname']. "';";
+		$result = connection()->query($sql);
+		if (mysqli_num_rows ($result) == 1) {
+			$allowedRoles = connection()->query($sql)->fetch_assoc()['roleLevel'];
+		}	
+	}
+	
+	try { 
+		if (!empty($_POST['uname']) and isset($_POST['pword']) and !isset($_POST['role'])) {
+			// unvalidated username and password entered
+		
+			// confirm valid username & get password hash, otherwise invalid username.
+			connection();
+			$sql = "SELECT userPasswordHash('" .$_POST['uname']. "') AS userPasswordHash;";
+			$passResult = connection()->query($sql)->fetch_assoc()['userPasswordHash'];
+
+			// confirm entered password matches stored password, otherwise invalid password entered.
+			if (!password_verify($_POST['pword'], $passResult)) {
+				$errorMessage = "Password Is Not Valid";
+			}
+		}
+		elseif (isset($_POST['role'])) {
+			// username, password, and role have been validated
+		
+			// generate session token
+			$sessionToken = password_hash($_POST['uname'] . $_POST['pword'], PASSWORD_BCRYPT);
+					
+			// login to database and set session token, otherwise sessionToken is already in use or incorrect role selected.
+			$sql = "CALL login('" .$_POST['uname']. "', " .$_POST['role']. ", '" .$sessionToken. "');";
+			connection()->query($sql);	
+			
+			// LOGIN SUCCESSFUL..... redirect to the appropriate page.
+			header("Location: ../ServerView/ServerView.php");
+			
+			
+			
+			
+			
+			setcookie($cookie_name, $sessionToken, time() + (86400 * 30), "/"); // 86400 is 1 day
+		}
+	}
+	catch (Exception $e) {
+		$errorMessage = $e->getMessage();
+	}
+?>
+<html>
+<head>
+	<title> CentRes Employee Portal </title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<div id="login-container">
+	<h2> CentRES Employee Portal </h2><br>
+
+	<div>
+		<form action="login.php" method="POST" id="frmLogin">
+
+			<label for="uname" id="lblLoginUsername">Enter Your Username</label>
+			<input type=text id='txtLoginUsername' name='uname' <?php if (isset($_POST['uname'])) { echo('value="' . $_POST['uname'] . '"');} echo( '>'); ?>
+			<br><br>
+			<label for="pword" id="lblLoginPassword">Enter Your Password</label>
+			<input type=password id='pwdLoginPassword' name='pword' <?php if (isset($_POST['pword'])) { echo('value="' . $_POST['pword'] . '"');} echo ('>'); ?>
+			<br><br>
+
+<?php		
+			
+			
+			
+			
+			if (isset($_POST['uname'])) {		
+				$sql = "SELECT loggedIn('" .$_POST['uname']. "') AS loggedIn";
+				try {
+					if (connection()->query($sql)->fetch_assoc()['loggedIn']) {
+						$errorMessage = "You are already logged in on another device or tab!";
+					}
+				}
+				catch (Exception $e) {
+					$errorMessage = $e->getMessage();
+				}
+				
+		
+				if (!isset($errorMessage)) {
+					$sql = "SELECT * FROM employeeroles";
+					$definedRoles = connection()->query($sql);
+			
+					echo "<label> Select Your Role </label>";
+					echo "<select name='role' id='cboLoginRole' onchange='autoLogin()'>";
+					echo "<option>Select Your Role</option>";
+					while($row = $definedRoles->fetch_assoc()) {
+						if( $row['id'] & $allowedRoles = $row['id']) {
+							echo ('<option value=' .$row['id']. '>' .$row['title']. ' - ' .$row['id']. '</option>');
+						}
+					}
+					echo "</select>";
+				
+					echo('<br><br>');
+				}
+				else {
+					echo('<input type="submit" value="Select Role" id="btnSelectRole">');
+				}
+			}
+			else {
+				echo('<input type="submit" value="Select Role" id="btnSelectRole">');
+			}
+			disconnect();
+			echo("<button id='btnClearLogin' onclick='clearLogin()' >Clear</button>
+			<script>
+				function autoLogin() {
+					document.getElementById('frmLogin').submit();
+				}
+				function clearLogin() {
+					document.getElementById('txtLoginUsername').remove();
+					document.getElementById('pwdLoginPassword').remove();
+					document.getElementById('cboLoginRole').remove();
+					
+					
+					autoLogin();
+					
+				}
+				
+				</script>
+			");
+			if (isset($errorMessage)) {
+				echo('<h1>' .$errorMessage. '</h1>');
+			}
+?>	
+
+		</form>	
+		<form action="login.php" method="POST" id="frmClearLogin">
+		</form>
+
+	</div>	
+
+
+	</div>
+
+</body>
+</html>
---
+++ CodeRepo/Resources/php/connect_disconnect.php
@@ -0,0 +1,55 @@
+<!-- 
+ File: connect_disconnect.php
+ This file contains two functions to be called for database connection/disconnection.
+ This allows for the connection code to be written once and used anywhere.
+ 
+ connection() can be used anywhere in you code after the statement that 
+ "includes" or "requires" connect_disconnect.php.
+ 
+ Always call disconnect() when you are done accessing the databse.
+ disconnect() an be called any amount of times and will not generate an error.
+-->
+
+<html>
+<body>
+<?php
+
+global $conn;
+global $connection_level;
+
+function connection() {
+	if ($GLOBALS['conn'] && !$GLOBALS['conn']->connect_error) {
+		$GLOBALS['connection_level']++;
+		return $GLOBALS['conn'];
+	}
+
+	$servername = "p:localhost";
+	$username = "scott";
+	$password = "tiger";
+	$dbname = "centres";
+
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+		} 
+
+	$GLOBALS['conn'] = $conn;
+	return $conn;
+}
+
+// Disconnect From The Persistent Database Connection. This Is Necessary
+function disconnect() {
+	try {
+		mysqli_close($GLOBALS['conn']);
+		
+		// coneection gracefully closed
+	}
+	catch (Exception $e) {
+	}
+	unset($GLOBALS['conn']);
+	
+}
+?>
+
+</body>
+</html>
