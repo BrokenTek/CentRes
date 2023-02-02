308c366cbd8b7c838d11604e65d65a6951ee4d0a
sashort <95879519+sashort@users.noreply.github.com>
2023-02-02 13:05:35 -0500

    Implements the logout button
    Logout button introduced and preliminary CSS introduced

--- CodeRepo/Resources/php/session.php
+++ CodeRepo/Resources/php/session.php
@@ -2,9 +2,13 @@
 	include 'connect_disconnect.php';
 	$cookie_name = "804288a34eb7a49b349be68fc6437621cbf25e10d82f4268bb795eca277adedb6a3367add5bfb7cbffb50df150e2e78d26b276f37d32d96cd76746065df58a30cde25c4d9803aa7214dc8f6a985bf8643c341f229b5834964b0f371915d5677e4b579fbab42844cd63ddc3148e4250591277cfc521906bc30cfedd765974c2009ae5fe451ab1890e5ebbfa120ad18934c972618dbe3e";
 	
+	//build a PHP Session class for use by calling/parent PHP files
+
+	$session_valid = false;
+	
 	//Cookie exists
 	if(isset($_COOKIE[$cookie_name])) {
-		$session_valid = false;
+		
 		try{
 			
 			// check corresponding accessToken in database exists and is valid
@@ -24,35 +28,10 @@
 			//check validated. 
 			$session_valid = true;
 			
-			//build a PHP Session class for use by calling/parent PHP files
-			class Session {
-				protected string $u;
-				protected string $f;
-				protected string $l;
-				protected int $r;
-				
-				public function __construct(string $username, string $last_name, string $first_name, int $role) {
-					$this->u = $username;
-					$this->l = $last_name;
-					$this->f = $first_name;
-					$this->r = $role;
-				}
-				
-				function username() {
-					return $this->u;
-				}
-				function last_name() {
-					return $this->l;
-				}
-				function first_name() {
-					return $this->f;
-				}
-				function role() {
-					return $this->r;
-				}
-			}
 			
-			$session = new Session($uname, $lname, $fname, $role);
+			$username = $uname;
+			$firstName = $fname;
+			$lastName = $lname;
 			
 			echo('
 				<script>
@@ -60,21 +39,55 @@
 					const FIRST_NAME = "' .$fname. '";
 					const LAST_NAME = "' .$lname. '";
 					const ROLE = ' .$role. ';
-				</script>
-			');
+				</script>'
+			);
+
+			echo('<link rel="stylesheet" href="../Resources/CSS/baseStyle.css">
+			<script>
+				function logout() {
+					
+					
+
+					let div = document.createElement("input");
+					div.setAttribute("type","hidden");
+					div.setAttribute("id", "aaaa");
+					div.setAttribute("name","logout");
+					div.setAttribute("value","true");
+					document.getElementsByClassName("sessionContainer")[0].append(div);
+					document.getElementsByClassName("sessionContainer")[0].submit();
+
+					
+					return;
+				}
+			</script>
+				<div class="sessionHeader">
+					<img src="../Resources/Images/centresLogo.png" id="lgoSession" width=50 height=50>
+					<div id="sessionDetails">' .$username. '</div>
+					<button type="button" id="btnLogout" onclick="logout()">Logout</button>
+				</div>');
+
+			
 		}
 		catch (Exception $e) {	
 			// cookie exists, but sessionID doesn't exist in the DB or it's expired.
-			
 			// remove the cookie
 			$_COOKIE[$cookie_name] = NULL;
 		}				
 			
 	}
-	
-	if (!$session_valid) {
+
+	if (!$session_valid or isset($_POST['logout'])) {
 		// redirect to the login page
 		header("Location: ../LoginView/Login.php");
-	}				
+
+		if (isset($_POST['logout'])) {
+			$sql = "CALL logout('" .$username. "');";
+			$result = connection()->query($sql);
+		}
+
+	}
+
+	unset($uname, $fname, $lname, $sql, $row);
+
 	disconnect();
 ?>
--- CodeRepo/ServerView/ServerView.php
+++ CodeRepo/ServerView/ServerView.php
@@ -1,110 +1,100 @@
-<?php require "../Resources/php/session.php"; ?>
 <html>
-   <head>
-	<link rel="stylesheet" href="../Resources/CSS/serverStructure.css">
-		
-  	  <script type="text/javascript">
-		function templateFunction( a ) { 
-			alert("You override the template function in your file"); 
-		}
-		
-		function reloadPage() {
-			document.getElementById("form-container").submit();
-		}
-		
-		function maxSeatNumber() { return 100; }
-		function maxSplitNumber() { return 9; }
-		function createMenuSelectEventHandlers() {}
-		function selectMenuItem( id ) {}
-		function selectTicketItem( id ) {}
-		function moveTicketItem() {}
-		function removeTicketItem() {}
-		function stateChanged() {}
-		function editTicketItem() {}
-		function configureModificationWindow() {}
-		function updateMenuItem() {}
-
-		
-
-  </script>
-  <script src="../InDev/cwpribble.js"></script>
-  <script src="../InDev/dbutshudiema.js"></script>
-  <script src="../InDev/dlmahan.js"></script>
-  <script src="../InDev/kcdine.js"></script>
-  <script src="../InDev/sashort.js"></script>
-  <script src="../InDev/OVERRIDEEXAMPLE.js"></script>
-  
-  <script>templateFunction("Hello World");</script>
-      <style>
-         
-         
-      </style>
-   <body>
-   <form id="serverContainer" action="ServerView.php" method="POST">
-		<div id="serverViewHeader">
-			<select id="cboTable" name="tableNumber" onchange="reloadPage()">
-				<option value="">Select Table</option>
-				<option value="1">Table 1</option>
-				<option value="2">Table 2</option>
-				<option value="3">Table 3</option>
-			</select>
-			<select id="cboSeat" name="seatNumber" onchange="stateChanged()">
-				<option value="">Select Seat</option>
-				<option value="1">Table 1</option>
-				<option value="2">Table 2</option>
-				<option value="3">Table 3</option>
-			</select>
-			<div id="headerButtonGroup">
-				<input type="submit" value="SUBMIT">
-				<button>CANCEL</button>
-				<button>PRINT RECIEPT</button>
-			</div>
-		</div>
-		<span id="serverViewContainer">
-		
-			<span id="menuTitle">Menu</span>
-			<span id="menuContainer">
-				<?php require "loadServerMenu.php"; ?>
-			</span>
-			
-			<span id="ticketHeader">
-				<span id="ticketHeaderText">Ticket&nbsp;Number&nbsp;Goes&nbsp;Here</span>
-				<select id="cboSplit">
-					
-				</select>
-			</span>
-			<span id="ticketContainer" style="display: initial;">
-				
-				<?php require "loadTicket.php"; 	 
-						if (!empty($_POST['tableNumber'])) {
-							loadTicketItems($_POST['tableNumber']);
-						}
-				?>
-				
-			</span>
-			<span id="modsContainer" style='display: none;'>
-				<?php require "loadModsWindow.php"; ?>
-			</span>
-			<span id="ticketFooter">
-				<button>Edit</button>
-				<span></span>
-				<button>Move To</button>
-				<button>Share With</button>
-				<select id="cboMoveTicketItem">
-					<option value="">Select Split</option>
-					<option value="Split 1">Split 1</option>
-					<option value="2">Split 2</option>
-					<option value="3" style="background-color: red;">Split 3</option>
-				</select>
-			</span>
-		</span>
-	  </form>
-	
-	<br>
-	<button type='button' id='getTicketContainer'>View Ticket Container</button>
-	<button type='button' id='getModsContainer'>View Mods Container</button>
-
-	<!-- Event Listeners: Currently Only Functions as a toggle between TICKETCONTAINER and MODSCONTAINER -->
-	<script src="../Resources/JavaScript/eventListeners.js"> </script>					
-   </body>
+    <head>
+        <link rel="stylesheet" href="../Resources/CSS/serverStyle.css">
+        <link rel="stylesheet" href="../Resources/CSS/serverStructure.css">
+        <script type="text/javascript">
+            function templateFunction( a ) { 
+            	alert("You override the template function in your file"); 
+            }
+            
+            function reloadPage() {
+            	document.getElementById("form-container").submit();
+            }
+            
+            function maxSeatNumber() { return 100; }
+            function maxSplitNumber() { return 9; }
+            function createMenuSelectEventHandlers() {}
+            function selectMenuItem( id ) {}
+            function selectTicketItem( id ) {}
+            function moveTicketItem() {}
+            function removeTicketItem() {}
+            function stateChanged() {}
+            function editTicketItem() {}
+            function configureModificationWindow() {}
+            function updateMenuItem() {}
+            
+        </script>
+        <script src="../InDev/cwpribble.js"></script>
+        <script src="../InDev/dbutshudiema.js"></script>
+        <script src="../InDev/dlmahan.js"></script>
+        <script src="../InDev/kcdine.js"></script>
+        <script src="../InDev/sashort.js"></script>
+        <script src="../InDev/OVERRIDEEXAMPLE.js"></script>
+        <script>templateFunction("Hello World");</script>
+    <body>
+        <form id="serverViewSession" class="sessionContainer" action="ServerView.php" method="POST">
+            <!-- session.php must be included after the opening for tag. It adds  -->
+            <?php require "../Resources/php/session.php"; ?>
+            <div id="serverViewContainer" class="sessionBody">
+                <div id="serverViewHeader">
+                    <select id="cboTable" name="tableNumber" onchange="reloadPage()">
+                        <option value="">Select Table</option>
+                        <option value="1">Table 1</option>
+                        <option value="2">Table 2</option>
+                        <option value="3">Table 3</option>
+                    </select>
+                    <select id="cboSeat" name="seatNumber" onchange="stateChanged()">
+                        <option value="">Select Seat</option>
+                        <option value="1">Table 1</option>
+                        <option value="2">Table 2</option>
+                        <option value="3">Table 3</option>
+                    </select>
+                    <div id="headerButtonGroup">
+						<button type="button" id="btnSubmit">SUBMIT</button>
+                        <button type="button" id="btnCancel">CANCEL</button>
+                        <button type="button id="btnPrintReceipt">PRINT RECIEPT</button>
+                    </div>
+                </div>
+                
+                    <div id="menuTitle">Menu</div>
+                    <div id="menuContainer">
+                    <?php require "loadServerMenu.php"; ?>
+                    </div>
+                    <div id="ticketHeader">
+                    <div id="ticketHeaderText">Ticket&nbsp;Number&nbsp;Goes&nbsp;Here</div>
+                    <select id="cboSplit">
+                    </select>
+                    </div>
+                    <div id="ticketContainer" style="display: initial;">
+                    <?php require "loadTicket.php"; 	 
+                        if (!empty($_POST['tableNumber'])) {
+                        	loadTicketItems($_POST['tableNumber']);
+                        }
+                        ?>
+                    </div>
+                    <div id="modsContainer" style='display: none;'>
+                    <?php require "loadModsWindow.php"; ?>
+                    </div>
+                    <div id="ticketFooter">
+                        <button type="button">Edit</button>
+                        <div></div>
+                        <button type="button" onclick="">Move To</button>
+                        <button type="button" onclick="">Split With</button>
+                        <select id="cboMoveTicketItem">
+                            <option value="">Select Split</option>
+                            <option value="Split 1">Split 1</option>
+                            <option value="2">Split 2</option>
+                            <option value="3" style="background-color: red;">Split 3</option>
+                        </select>
+                    </div>
+                
+            </div>
+        </form>
+        <!-- TEMPORARY buttons to toggle between TICKETCONTAINER and MODSCONTAINER -->
+        <br>
+        <button type='button' id='getTicketContainer'>View Ticket Container</button>
+        <button type='button' id='getModsContainer'>View Mods Container</button>
+        <!-- Event Listeners: Currently Only Functions as a toggle between TICKETCONTAINER and MODSCONTAINER -->
+        <script src="../Resources/JavaScript/eventListeners.js"> </script>
+    </body>
 </html>
