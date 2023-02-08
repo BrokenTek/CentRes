bca54bf477378a35436f59a09c3e3435d319531a
dbutshudiema <dbutshudiema@my.waketech.edu>
2023-02-08 13:22:18 -0500

    added event listener

---
+++ CodeRepo/InDev/dbutshudiema.js
@@ -0,0 +1,4 @@
+//added event listeners for selecting ticket item
+document.getElementById("ticketItem_' . $ticketItem['id'] . '").addEventListener("click", window.selectTicketItem);
+//added event listeners for removing ticket item
+document.getElementById("ticketItem_' . $ticketItem['id'] . '").addEventListener("contextmenu", window.removeTicketItem);
