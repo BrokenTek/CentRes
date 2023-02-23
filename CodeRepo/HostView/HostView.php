ef228173931cff59bad52712c1db7644cd67c03c
sashort <95879519+sashort@users.noreply.github.com>
2023-02-22 23:39:24 -0500

    Introduces HostView.php skeleton
    Changes TableStatus Unavailable to Disabled

--- CodeRepo/HostView/HostView.php
+++ CodeRepo/HostView/HostView.php
@@ -1,3 +1,58 @@
 <?php
-	echo "Hello Sprint 2";
+	include '../Resources/php/sessionLogic.php';
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			.sessionBody {
+				display: grid;
+				grid-template-areas: "ifrServerList ifrRestaurantLayout ifrSelectedTable"
+									 "ifrWaitList   ifrRestaurantLayout ifrTicket"
+									 "ifrWaitTimes  ifrRestaurantLayout ifrTicket";
+				grid-template-columns: 1fr 3fr 1fr;
+				grid-template-rows: 2fr 2fr 1fr;
+			}
+
+			#ifrServerList {
+				grid-area: ifrServerList;
+			}
+
+			#ifrWaitList {
+				grid-area: ifrWaitList;
+			}
+
+			#ifrWaitTimes {
+				grid-area: ifrWaitTimes;
+			}
+
+			#ifrSelectedTable {
+				grid-area: ifrSelectedTable;
+			}
+
+			#ifrTicket {
+				grid-area: ifrTicket;
+			}
+
+			#ifrRestaurantLayout {
+				grid-area: ifrRestaurantLayout;
+			}
+		</style>
+		<script>
+			<!-- event loop goes here -->
+		</script>
+	</head>
+	<body>
+		<form  id="hostViewSession" class="sessionContainer">
+			<?php require "../Resources/php/sessionHeader.php"; ?>
+			<div class="sessionBody">
+				<iframe id="ifrServerList" src="ServerList.php" frameborder='0' width="100%" height="100%"></iframe>
+				<iframe id="ifrWaitList" src="WaitList.php" frameborder='0' width="100%" height="100%"></iframe>
+				<iframe id="ifrWaitTimes" src="WaitTimes.php" frameborder='0' width="100%" height="100%"></iframe>
+				<iframe id="ifrSelectedTable" src="SelectedTable.php" frameborder='0' width="100%" height="100%"></iframe>
+				<iframe id="ifrTicket" src="../Resources/php/ticket.php" frameborder='0' width="100%" height="100%"></iframe>
+				<iframe id="ifrRestaurantLayout" src="RestaurantLayout.php" frameborder='0' width="100%" height="100%"></iframe>
+			</div>
+		</form>
+	</body>
+</html>
