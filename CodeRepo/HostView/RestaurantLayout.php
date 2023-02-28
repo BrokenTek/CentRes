72fbdff3819ebe866363336e5827098c794c60ec
BrokenTek <cwpribble@my.waketech.edu>
2023-02-28 04:15:16 -0500

    Script for tableshapes/RestaurantView.php w/ four tables
    This is for testing and the script will add the records into the tableshapes table.

--- CodeRepo/HostView/RestaurantLayout.php
+++ CodeRepo/HostView/RestaurantLayout.php
@@ -22,17 +22,43 @@
 </head>
 
 <body>
+    <svg id='parentSvg' xmlns="http://www.w3.org/2000/svg" height="100vh" width="100vw">
+    <script type="application/ecmascript">
+        <![CDATA[
+            function transformMe(evt) {
+                // svg root element to access the createSVGTransform() function
+                const svgroot = evt.target.parentNode;
+                // SVGTransformList of the element that has been clicked on
+                const tfmList = evt.target.transform.baseVal;
 
-    <link rel="stylesheet" href="styles.css"></style>
-    
-    <svg viewBox="0 0 1280 720">
+                // Create a separate transform object for each transform
+                const translate = svgroot.createSVGTransform();
+                translate.setTranslate(50,5);
+                const rotate = svgroot.createSVGTransform();
+                rotate.setRotate(10,0,0);
+                const scale = svgroot.createSVGTransform();
+                scale.setScale(0.8,0.8);
 
-            <svg onpointerdown='clickVerify()' id="L01" class="longtable" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect class="open" width="5%" height="5%" fill="grey" stroke="black" stroke-opacity="0.75" />                  
-            </svg>
+                // apply the transformations by appending the SVGTransform objects to the SVGTransformList associated with the element
+                tfmList.appendItem(translate);
+                tfmList.appendItem(rotate);
+                tfmList.appendItem(scale);
+        }
+        ]]>
+    </script>
 
+    <path id="TABLEID" width="5vmin" height="10vmin" class="tableshape booth" d="M1 16V1H14.9535V16M1 16V31H14.9535V16M1 16H14.9535" fill="#808080" stroke="black" stroke-opacity="0.75" transform="translate(0 0)" />
+    <circle id="TABLEID" class="tableshape hightop" width="10vmin" height="10vmin" cx="5vmin" cy="5vmin" r="5vmin" fill="grey" stroke="black" stroke-opacity="0.75" transform="translate(0 100)" />
+    <rect id="TABLEID" class="tableshape longtable" width="10vmin" height="5vmin" fill="grey" stroke="black" stroke-opacity="0.75" transform="translate(0 200)" />
+    <rect id="TABLEID" class="tableshape square" width="10vmin" height="10vmin" fill="grey" stroke="black" stroke-opacity="0.75" transform="translate(0 300)" />
 
+00 00
     </svg>
+
+    <script>
+        
+        // window.addEventListener('DOMContentLoaded', setDimensions);
+    </script>
 </body>
 </html>
 
