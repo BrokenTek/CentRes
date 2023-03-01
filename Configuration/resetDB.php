079c1891828ca3753c9d8e43af377612656b5c55
BrokenTek <cwpribble@my.waketech.edu>
2023-03-01 00:48:24 -0500

    Comments added to config script
    Comments added for the proverbial future developer that, in real production, would work on this source code in the future without any input from the original creators outside of the comments and documentation. This is a common issue with legacy codebases and the more comments, the better (to an extent: blocks should typically always be prefixed with a comment. Variable naming should be as indicative of the contents as possible, but not discursive). The introduction of a new convention for comments/statements that are left in a state that is not intended to be inveterate on production. Within a comment meant to indicate a stopgap, the exact string, (to be removed), should be added so all occurrences of temporary solutions/comments can be found and removed/changed via Find. In VS Code, 'CTRL+Shift+F 'on the CentRes directory will find these instances, making the digging for necessary removals much easier before the final deliverable is integrated and polished.

--- Configuration/resetDB.php
+++ Configuration/resetDB.php
@@ -1,3 +1,9 @@
+<!-- (to be removed) SUGGESTION: Comments added to the JS
+    For the discovery of comments that are to be removed upon
+    the conclusion of integration testing (or before), use the
+    find in folder functionality (CTRL+Shift+F) to find all occurances 
+    in all files in a folder of the text string: (to be removed) 
+    **use this on CentRes folder to search all**  -->
 <html>
     <head>
         <script>
@@ -15,6 +21,7 @@
         </form>
         <?php
             try {
+                // Connect to the database using the authentication parameters defined below
                 $mysql_host = "localhost";
                 $mysql_database = "Centres";
                 $mysql_user = "scott";
@@ -22,27 +29,32 @@
                 $dbh= new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
                 $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
                 
+                // Recreate/Update the database tables strucuture
                 $query = file_get_contents("CentResCreateDB.txt");
                 $dbh->exec($query);
                 print("<h1>☑ Database Structure Updated</h1>");
                 
-                
+                // Recreate/Update the database functions and procedures
                 $query = file_get_contents("CentResDBFunctionsAndMethods.txt");
                 $dbh->exec($query);
                 print("<h1>☑ Database Functions Updated</h1>");
 
+                // Recreate/Update the table 'tableshapes' records
                 $query = file_get_contents("CentResPopulate_tableshapes.txt");
                 $dbh->exec($query);
                 print("<h1>☑ Populated Tables</h1>");
 
+                // Recreate/Update employees and related records
                 $query = file_get_contents("LoadThreeStooges.txt");
                 $dbh->exec($query);
                 print("<h1>☑ Loaded 3 Stooges as Employees</h1>");
 
+                // Message to verify the success of all queries in the block
                 echo("<script>resetSuccessful = true;</script>");
 
+            // If one of the queries in the try block fail, the rest are not run. Message below will appear
             } catch(Exception $e) {
-                print("<h1>☒ " .$e->getMessage(). "</h1>");//Remove or change message in production code
+                print("<h1>☒ " .$e->getMessage(). "</h1>"); // (to be removed) Remove or change message in production code
             }
     ?>
     </body>
